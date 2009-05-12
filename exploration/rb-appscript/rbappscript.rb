1659ebe38f9041fc7b5b27242e5af60083134838
Luke Bayes <lbayes@patternpark.com>
2009-05-12 15:09:12 -0700

    Tried last approach with player 10

--- exploration/rb-appscript/rbappscript.rb
+++ exploration/rb-appscript/rbappscript.rb
@@ -2,9 +2,15 @@
 require 'rubygems'
 require 'appscript'
 
-player = "/Users/lbayes/Projects/CLIXFlashPlayer/exploration/fixtures/9.0.151/Flash Player.app/Contents/MacOS/Flash Player"
+player9 = "/Users/lbayes/Projects/CLIXFlashPlayer/exploration/fixtures/9.0.151/Flash Player.app/Contents/MacOS/Flash Player"
+player10 = "/Users/lbayes/Projects/CLIXFlashPlayer/exploration/fixtures/10.0.22.87/Flash Player.app/Contents/MacOS/Flash Player"
+
+player = player10
+
 good_swf = '/Users/lbayes/Projects/CLIXFlashPlayer/exploration/fixtures/SomeProject.swf'
 bad_swf = '/Users/lbayes/Projects/CLIXFlashPlayer/exploration/fixtures/InstantRuntimeException.swf'
+
+swf = good_swf
 
 # Open the player direectly without a SWF file:
 player_thread = Thread.new {
@@ -16,17 +22,10 @@ player_thread = Thread.new {
 activate_thread = Thread.new {
   Appscript.app(player).activate
   puts "activate succeeded"
-}
-
-puts "working"
-
-open_thread = Thread.new {
-  Appscript.app(player).open(MacTypes::Alias.path(good_swf))
+  Appscript.app(player).open(MacTypes::Alias.path(swf))
   puts "open succeeded"
 }
 
 puts "Player Launched"
-open_thread.join
 player_thread.join
-
 puts "Player Returned"
