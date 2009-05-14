3e59a6e8534141b75edcc1ab3288d61b0ecad0d5
Luke Bayes <lbayes@patternpark.com>
2009-05-14 09:54:43 -0700

    Pulled unused trace statements

--- lib/clix_flash_player.rb
+++ lib/clix_flash_player.rb
@@ -14,22 +14,15 @@ class CLIXFlashPlayer
     @thread = Thread.new {
       player = File.expand_path(player)
       swf = File.expand_path(swf)
-      puts "------------------"
-      puts "CLIXFlashPlayer.execute with: #{player} and #{swf}"
       @player_pid = open4.popen4("#{player.split(' ').join('\ ')}")[0]
-      puts "Player started with player_pid: #{@player_pid}"
       wrapper = File.expand_path(File.dirname(__FILE__) + '/clix_wrapper.rb')
       command = "ruby #{wrapper} '#{player}' '#{swf}'"
-      puts command
       @activate_pid = open4.popen4(command)[0]
-      puts "Activate started with activate_id: #{@activate_pid}"
-      puts "Waiting for Player Close"
       Process.wait(@player_pid)
     }
   end
   
   def kill
-    puts "kill with: #{@player_pid}"
     exec("kill -9 #{@player_pid}")
   end
   
